71bb4a906481dc6db173647595e0eebae8b03681
gladgaeun <glad@ewha.ac.kr>
2025-04-06 21:42:38 +0900

    3주차 실습-1

---
+++ chapter3-movie/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MoviePage from './pages/MoviePage';
+
+function App() : Element {
+  return (
+    <>
+      <MoviePage />
+    </>
+  );
+}
+
+export default App;
---
+++ chapter3-movie/src/types/movie.ts
@@ -0,0 +1,22 @@
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  popularity: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type MovieResponse = {
+  page: number;
+  results: [];
+  total_pages: number;
+  total_results: number;
+};
