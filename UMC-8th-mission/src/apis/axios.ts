38656a05bcb6fed74b2ed745360042f5672bf440
gladgaeun <glad@ewha.ac.kr>
2025-05-12 15:15:35 +0900

    4주차 미션-2,3(에러 해결)

--- UMC-8th-mission/src/apis/axios.ts
+++ UMC-8th-mission/src/apis/axios.ts
@@ -4,7 +4,21 @@ import { LOCAL_STORAGE_KEY } from "../constants/key";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY.accessToken)}`,
-  },
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY.accessToken)}`,
+  // },
 });
+
+// 요청 인터셉터 추가
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
+    if (token) {
+      config.headers.Authorization = `Bearer ${JSON.parse(token)}`; // 토큰이 JSON 문자열이면 파싱
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
